eb2890731842520482ffb8724b2084937898b634
Addipalli Siva Sai <Addipalli.Sivasai@ltts.com>
2024-08-02 19:13:25 +0530

    test file updates

--- HelloWorldMaven/src/test/java/com/test/HelloWorldMaven/CalculateTest.java
+++ HelloWorldMaven/src/test/java/com/test/HelloWorldMaven/CalculateTest.java
@@ -1,30 +1,8 @@
+import junit.framework.Test;
 import junit.framework.TestCase;
  
-public class CalculatorTest extends TestCase {
-    private Calculator calculator;
- 
-    public CalculatorTest(String name) {
-        super(name);
+public class MyTest extends TestCase {
+    public void testAssertEquals() {
+        assertEquals(2, 2);
     }
- 
-    public static Test suite() {
-        return new TestSuite(CalculatorTest.class);
-    }
- 
-    public void setUp() {
-        calculator = new Calculator();
-    }
- 
-    public void tearDown() {
-        calculator = null;
-    }
- 
-    public void testAdd() {
-        int result = calculator.add(2, 2);
-        assertEquals(4, result);
-    }
- 
-    public void testSubtract() {
-        int result = calculator.subtract(4, 2);
-        assertEquals(2, result);
-    }
+}
