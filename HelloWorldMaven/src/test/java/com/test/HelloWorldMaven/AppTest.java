31b533a88481e3ece3dbc07ff23eeb9692eb50e8
Addipalli Siva Sai <Addipalli.Sivasai@ltts.com>
2024-08-03 12:55:39 +0530

    added report in jenkins

--- HelloWorldMaven/src/test/java/com/test/HelloWorldMaven/AppTest.java
+++ HelloWorldMaven/src/test/java/com/test/HelloWorldMaven/AppTest.java
@@ -2,8 +2,7 @@ package com.test.HelloWorldMaven;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
 /**
  * Unit test for simple App.
  */
--- HelloWorldMaven/src/test/java/com/test/HelloWorldMaven/CalculateTest.java
+++
@@ -1,8 +0,0 @@
-import junit.framework.Test;
-import junit.framework.TestCase;
- 
-public class MyTest extends TestCase {
-    public void testAssertEquals() {
-        assertEquals(2, 2);
-    }
-}
