11129c95f411c61bc6cd6c5634066593c1ca2007
Addipalli Siva Sai <Addipalli.Sivasai@ltts.com>
2024-08-03 15:07:09 +0530

    some check styles are cleared

--- HelloWorldMaven/src/main/java/com/test/HelloWorldMaven/App.java
+++ HelloWorldMaven/src/main/java/com/test/HelloWorldMaven/App.java
@@ -3,7 +3,7 @@ final class App {
     private App() {
         throw new UnsupportedOperationException("Utility class");
     }
-        public static void main(final String[] args){
+        public static void main(final String[] args) {
             int a;
             System.out.println("Hello World!!!!!");
     }
