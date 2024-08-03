c6680f5d20f732b9b516337e7658f736399800e3
Addipalli Siva Sai <Addipalli.Sivasai@ltts.com>
2024-08-03 14:39:37 +0530

    calculator app deleted

--- HelloWorldMaven/src/main/java/com/test/HelloWorldMaven/App.java
+++ HelloWorldMaven/src/main/java/com/test/HelloWorldMaven/App.java
@@ -1,6 +1,6 @@
 package com.test.HelloWorldMaven;
-public class App {
-    public static void main(String[] args){
-        System.out.println("Hello World!!!!!");
+final class App {
+        public static void main(String[] args) {
+            System.out.println("Hello World!!!!!");
     }
 }
