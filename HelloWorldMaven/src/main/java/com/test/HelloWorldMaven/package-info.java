4a4971805147db932ff950e58e6cafd45070579e
Addipalli Siva Sai <Addipalli.Sivasai@ltts.com>
2024-08-03 14:59:49 +0530

    some check styles are cleared

--- HelloWorldMaven/src/main/java/com/test/HelloWorldMaven/package-info.java
+++ HelloWorldMaven/src/main/java/com/test/HelloWorldMaven/package-info.java
@@ -1,1 +1,8 @@
+/**
+ * This package contains the HelloWorldMaven application.
+ * <p>
+ * It includes the main class {@link com.test.HelloWorldMaven.App} which serves
+ * as the entry point.
+ * </p>
+ */
 package com.test.HelloWorldMaven;
