8c324828c0780923aac3a740b9ec85511d389b12
Addipalli Siva Sai <Addipalli.Sivasai@ltts.com>
2024-08-06 22:19:06 +0530

    new line of code is added

--- HelloWorldMaven/src/main/java/com/test/HelloWorldMaven/App.java
+++ HelloWorldMaven/src/main/java/com/test/HelloWorldMaven/App.java
@@ -6,5 +6,16 @@ final class App {
         public static void main(final String[] args) {
             int a;
             System.out.println("Hello World!!!!!");
+            for(int i=0;i<10;i++)
+            {
+                System.out.println(i);
+
+            }
+            for(int i=0;i<10;i++)
+            {
+                System.out.println(i);
+
+            }
+            
     }
 }
